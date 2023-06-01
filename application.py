83564a692ec49ade1e14c0e7986f05a03a4bc651
trand <kieutrungtran@gmail.com>
2023-06-01 08:37:34 -0400

    load pickle

--- application.py
+++ application.py
@@ -1,5 +1,10 @@
 from flask import Flask, request
+import joblib
 app = Flask(__name__)
+
+vectorizer = joblib.load("a_vectorizer.pkl")
+spam_ham_model = joblib.load("spam_ham_model.pkl")
+
 
 @app.route('/')
 def hello_world():
@@ -8,7 +13,9 @@ def hello_world():
 @app.route('/spamorham', methods=['GET', 'POST'])
 def spamorham():
     message = request.args.get('message')
-    return message
+    vect_message = vectorizer.transform([message])
+    result = spam_ham_model.predict(vect_message)[0]
+    return result
 
 if __name__=='__main__':
     app.run()
